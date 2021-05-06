d6b5c29174de5ce2df5ea12ad5d02d3a71b410e7
Zuzanna <zuzanna@Zuzannas-MacBook-Air.local>
2021-05-06 12:04:19 +0100

    added express sessions

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const session = require('express-session')
 const methodOverride = require('method-override')
 
+
 app.use(express.urlencoded({ extended: true }))
+app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: false, cookie: {secure: false}}))
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 
 const indexController = require('./controllers/index.js')
+const signupController = require('./controllers/signup.js')
 const bookmarksController = require('./controllers/bookmarks.js')
 const commentsController = require('./controllers/comments.js')
 const tagsController = require('./controllers/tags.js')
 
 app.use('/', indexController)
+app.use('/signup', signupController)
 app.use('/bookmarks', bookmarksController)
 app.use('/bookmarks/:bookmarkId/comments', commentsController)
 app.use('/tags', tagsController)
+
 
 
 app.listen(port, () => {
--- cypress/integration/user-journey.spec.js
+++ cypress/integration/user-journey.spec.js
@@ -2,57 +2,57 @@ describe("User journey tests:", function(){
 
     it("Go to index page, should see sign up link:", function(){
         cy.visit('/')
-        cy.get('#sign-up-link').should('contain','Sign Up')  
+        cy.get('#sign-up-link').should('contain','Sign Up')
     })
 
     it("Go to bookmarks, not sign in, should be redirected to index:", function(){
         cy.visit('/bookmarks')
-        cy.get('#sign-up-link').should('contain','Sign Up') 
+        cy.get('#sign-up-link').should('contain','Sign Up')
     })
 
     it("Go to tags, not sign in, should be redirected to index:", function(){
         cy.visit('/tags/:name')
-        cy.get('#sign-up-link').should('contain','Sign Up') 
+        cy.get('#sign-up-link').should('contain','Sign Up')
     })
 
     it("Go to index page, click sign up link:", function(){
         cy.visit('/')
-        cy.get('#sign-up-link').should('contain','Sign Up')  
+        cy.get('#sign-up-link').should('contain','Sign Up')
         cy.get('#sign-up-link').click()
         cy.get('#email-textbox').should('be.visible')
         cy.get('#password-textbox').should('be.visible')
         cy.get('#submit-button').should('contain','Submit')
-        cy.url().contains('/signup') 
+        cy.url().should('contain', '/signup')
     })
 
     it("Fill sign up form correctly, should go to bookmarks:", function(){
         cy.visit('/')
-        cy.get('#sign-up-link').should('contain','Sign Up')  
+        cy.get('#sign-up-link').should('contain','Sign Up')
         cy.get('#sign-up-link').click()
         cy.get('#email-textbox').type('jj@hotmail.com')
         cy.get('#password-textbox').type('1111')
         cy.get('#submit-button').click()
-        cy.url().contains('/bookmarks') 
+        cy.url().should('contain', '/bookmarks')
     })
 
     it("Fill sign up form incorrectly, should go to bookmarks:", function(){
         cy.visit('/')
-        cy.get('#sign-up-link').should('contain','Sign Up')  
+        cy.get('#sign-up-link').should('contain','Sign Up')
         cy.get('#sign-up-link').click()
         cy.get('#email-textbox').type('jj')
         cy.get('#password-textbox').type('1111')
         cy.get('#submit-button').click()
-        cy.url().contains('/signup') 
+        cy.url().should('contain', '/signup')
     })
 
     it("Can navigate the app once signed in correctly:", function(){
         cy.visit('/')
-        cy.get('#sign-up-link').should('contain','Sign Up')  
+        cy.get('#sign-up-link').should('contain','Sign Up')
         cy.get('#sign-up-link').click()
         cy.get('#email-textbox').type('jj@hotmail.com')
         cy.get('#password-textbox').type('1111')
         cy.get('#submit-button').click()
-        cy.url().contains('/bookmarks') 
+        cy.url().should('contain', '/bookmarks')
         cy.get('#bookmark-textbox').type('google.com')
         cy.get('#bookmark-submit').click()
         cy.contains('google.com')
